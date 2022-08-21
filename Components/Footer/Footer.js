57ad033317f3ba52e61404dca1e4b371ff40c0d7
ANIRUDHADITHYA <anirudhadithya.b.s@gmail.com>
2022-08-21 21:31:13 +0530

    Updated

---
+++ Components/Footer/Footer.js
@@ -0,0 +1,38 @@
+
+
+
+const Footer = () => {
+    return(
+        <div className="footerSection">
+            <div className="footerContainer">
+                <div className="footerHeader">
+                    <h1>Join our list</h1>
+                    <p>
+                        Signup to be the first to hear about exclusive deals, special offers and upcoming collections
+                    </p>
+                </div>
+                <div className="footerEmailSection">
+                    <input placeholder="Enter your email address"></input>
+                    <button>Download</button>
+                </div>
+                <div className="footerMain">
+                    <div className="footerFlex left">
+                        <p>@2022 Valence All rights reserved</p>
+                    </div>
+                    <div className="footerFlex">
+                        <p><span>Privacy Policy</span><span>FAQ's</span><span>Contact Us</span></p>
+                    </div>
+                    <div className="footerFlex right">
+                        <p><i class="fa-brands fa-linkedin-in"></i></p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../Components/Navbar/Navbar'
+import Footer from '../Components/Footer/Footer'
 
 export default function Home() {
   return (
@@ -12,6 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <div className='HomeSampleBody'>
+        <h1>Hello World</h1>
+      </div>
+      <Footer />
     </div>
   )
 }
